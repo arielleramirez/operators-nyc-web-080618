2f3984479b3b08093821a52b9368e0dac1e33958
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 13:32:02 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,26 @@
-def unsafe?(speed)
 
+def unsafe?(speed)
+ if speed > 60 
+   return true
+ end
+ 
+ if speed < 40 
+  return true 
+end 
+
+else
+  return false 
 end
 
 
 
 def not_safe?(speed)
-	
+  speed > 60 && speed < 40 ? true : false
+  if not_safe == 50 
+ 
 end
+
 	
+
 
 
